890864c6f13e8891dbc7f4421e0ec9b67dd82c8c
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-07-19 12:29:17 +0200

    story: added commented user story for page

--- src/stories/021_generate_page/index.ts
+++ src/stories/021_generate_page/index.ts
@@ -1,3 +1,23 @@
 export default function initPageStory() {
 	console.log("You chose to create a page")
+
+	//0. ask if file is a page, error, loading, layout or template
+	//1. ask for path -> ex: about
+	//2. ask for file path : do the user want to update it ?
+	// 	- yes : user prompts the new path ex: app/(pages)/about
+	// 	- no : continue
+	//3. check if the path to file exists : if not, create it. continue
+	//4. check if the file page/template/loading/layout/error exists : if exists, ask if the user wants to overwrite it ?
+	// 	- yes : continue
+	// 	- no : exit
+	//5. if file is a page : if not go to step 6.
+	//  5.1 Does the page fetches data ?
+	// 		- yes : does the user wants to cache the data ?
+	// 			- yes : will add a fetch and cache template
+	// 			- no : will add a fetch template without caching
+	// 		- no : continue
+	// 	5.2 Does the user wants to update the page metadata ?
+	// 		- yes : will add a metadata template
+	// 		- no : continue
+	//6. Success ex: add the file  with the options and proper template at app/(pages)/about/file-type.tsx/template.tsx/error.tsx ...
 }
--- src/stories/022_generate_component/index.ts
+++ src/stories/022_generate_component/index.ts
@@ -1,3 +1,22 @@
 export default function initComponentStory() {
 	console.log("You chose to create a component")
+
+	//1. ask for the component's name -> ex: Button
+	//2. ask for file path : do the user want to update it ?
+	// 	- yes : user prompts the new path ex: app/(pages)/about
+	// 	- no : continue
+	//3. check if the path to file exists : if not, create it. continue
+	//4. check if the file page/template/loading/layout/error exists : if exists, ask if the user wants to overwrite it ?
+	// 	- yes : continue
+	// 	- no : exit
+	//5. if file is a page : if not go to step 6.
+	//  5.1 Does the page fetches data ?
+	// 		- yes : does the user wants to cache the data ?
+	// 			- yes : will add a fetch and cache template
+	// 			- no : will add a fetch template without caching
+	// 		- no : continue
+	// 	5.2 Does the user wants to update the page metadata ?
+	// 		- yes : will add a metadata template
+	// 		- no : continue
+	//6. Success ex: add the file  with the options and proper template at app/(pages)/about/file-type.tsx/template.tsx/error.tsx ...
 }
