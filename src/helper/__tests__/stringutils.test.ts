64d3d9ccc57583275ffdbcd427a22a046f479c71
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-07-19 18:37:25 +0200

    implemented tests ❌ but they don't pass

--- src/helper/__tests__/stringutils.test.ts
+++ src/helper/__tests__/stringutils.test.ts
@@ -1,3 +1,22 @@
-test("is true", () => {
-	expect(true).toBe(true)
+import {sanitizeMessage} from "../stringUtils"
+import path from "path"
+
+describe("When sanitizeMessage is called", () => {
+	const systemSeparator = path.sep
+
+	it("with a valid parameter", () => {
+		expect(sanitizeMessage(`[:hello]`)).toEqual(`[:hello]`)
+	})
+	it("with invalid characters", () => {
+		expect(() => sanitizeMessage(`hel@$%#lo`)).toThrowError("Invalid message")
+	})
+	it("called with a valid path", () => {
+		expect(sanitizeMessage(`${systemSeparator}hello`)).toEqual(`hello`)
+	})
+	it("is called with a valid path", () => {
+		expect(sanitizeMessage(`hello${systemSeparator}`)).toEqual(`hello`)
+	})
+	it("is called with a valid group", () => {
+		expect(sanitizeMessage("(group)/hello")).toEqual("(group)/hello")
+	})
 })
--- src/helper/stringUtils.ts
+++ src/helper/stringUtils.ts
@@ -1,21 +1,65 @@
 import path from "path"
 
 export function sanitizeMessage(message) {
-	const firstCharRegex = /^[^a-zA-Z([{]/
-	const lastCharRegex = /[^a-zA-Z0-9)\]}]$/
-	const middleCharsRegex = /[^a-zA-Z0-9.\[\](){}]|(\.\.)+|(\/)+/g
+	const systemSeparator = path.sep
+	const oppositeSeparator = systemSeparator === "/" ? "\\" : "/"
+	const invalidChars = /[\u0300-\u036f]|[0-9!<>@#$%^&*{}'";:,_\-\s+=]+/g
 
-	// Sanitize first character
-	message = message.replace(firstCharRegex, "")
-	// Sanitize last character
-	message = message.replace(lastCharRegex, "")
-	// Sanitize middle characters
-	message = message.replace(middleCharsRegex, match => {
-		if (match === ".." || match === path.sep) {
-			return match
-		}
-		return ""
-	})
+	const initialMessage = message
 
-	return message
+	// clean up message
+	message = message.trim().normalize("NFD")
+	message.startsWith(systemSeparator) && (message = message.slice(1))
+	message.endsWith(systemSeparator) && (message = message.slice(0, -1))
+
+	// check if message is a parameter
+	const isParam =
+		message.startsWith("[") &&
+		message.endsWith("]") &&
+		message.length > 5 &&
+		message[1] === ":" &&
+		!message.includes(" ") &&
+		!message.includes(systemSeparator) && // 👈 change this
+		!message.includes(oppositeSeparator) && // 👈 and this
+		!invalidChars.test(message)
+
+	console.log(message, "starts with [", message.startsWith("["))
+	console.log(message, "ends with ]", message.endsWith("]"))
+	console.log(message, "lenght > 5", message.length > 5)
+	console.log(message, "has :", message[1] === ":")
+	console.log(message, "has no white space", !message.includes(" "))
+	console.log(message, "has no /", !message.includes(systemSeparator))
+	console.log(message, "has no \\", !message.includes(oppositeSeparator))
+	console.log(message, "do not matches invalid chars", !invalidChars.test(message))
+	// check if message is a group
+	const isGroup =
+		message.startsWith("(") &&
+		message.endsWith(")") &&
+		message.length > 4 &&
+		!message.includes(" ") &&
+		!message.includes(systemSeparator) && // 👈 change this
+		!message.includes(oppositeSeparator) && // 👈 and this
+		!invalidChars.test(message)
+
+	// check if message is a redirection
+	const isRedirection =
+		message.startsWith("(") &&
+		message.endsWith(")") &&
+		message.length > 4 &&
+		!message.includes(" ") &&
+		!invalidChars.test(message)
+
+	// check if message is a path
+	const isPath =
+		message.match(/^[a-zA-Z]/) &&
+		message.match(/[a-zA-Z]$/) &&
+		message.length > 3 &&
+		!message.includes(oppositeSeparator) &&
+		!invalidChars.test(message)
+
+	console.table({initialMessage, message, isParam, isGroup, isRedirection, isPath})
+
+	if (isGroup || isRedirection || isPath || isParam) {
+		return message
+	} else throw new Error("Invalid message")
 }
